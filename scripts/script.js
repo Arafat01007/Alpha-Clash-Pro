20f9ff0cfa73105e6b3fa5eaf6c39f9ad00cb942
Arafat01007 <arafatfahadbro@gmail.com>
2025-01-02 16:00:51 +0600

    added important functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,38 @@
-function play(){
-    
+// hide element by id
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
 }
+
+// show element by id
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+// continue
+function continueGame() {
+    // generate random alphabet
+    const alpaStr = 'abcdefghijklmnopqrstuvwxyz';
+    const alphas = alpaStr.split('');
+    const randomNum = Math.random() * 25;
+    const index = Math.round(randomNum);
+    const alphabet = alphas[index];
+    const scrAlpha = document.getElementById('screen-alpha');
+    scrAlpha.innerText = alphabet;
+    // bg color setting
+    addBgColorById(alphabet);
+}
+
+function play() {
+    hideElementById('home');
+    showElementById('playground');
+    continueGame();
+}
+// add bg color by id
+function addBgColorById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
+
+}
+
