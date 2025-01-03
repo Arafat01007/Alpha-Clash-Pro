65db1357c412c4d88ec851b6defdb71387fa35ff
Arafat01007 <arafatfahadbro@gmail.com>
2025-01-03 13:26:20 +0600

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -27,6 +27,9 @@ function continueGame() {
 function handleKeyPress(event) {
     const pressedKey = event.key;
     console.log('Player pressed', pressedKey);
+    if(pressedKey==='Escape'){
+        gameOver();
+    }
 
     const currAlphaML = document.getElementById('screen-alpha');
     const crAlpha = currAlphaML.innerText;
@@ -37,14 +40,48 @@ function handleKeyPress(event) {
         console.log('get a point');
         continueGame();
         removeBgColorById(pressedKey);
-
+        const currentScoreElement = document.getElementById('score');
+        const currScoreText = currentScoreElement.innerText;
+        const currScore = parseInt(currScoreText);
+        const newScore = currScore + 1;
+        currentScoreElement.innerText = newScore;
     }
     else {
         console.log('loss');
-        addRColorById(pressedKey);
+        const lifeElement = document.getElementById('life');
+        const lifeText = lifeElement.innerText;
+        const life = parseInt(lifeText);
+        const newLife = life - 1;
+        if (newLife === 0) {
+            gameOver();
+        }
+        lifeElement.innerText = newLife;
     }
 
 }
+
+
+
+// play again function
+function playAgain() {
+    hideElementById('score-screen');
+    showElementById('playground');
+    const lifeElement = document.getElementById('life');
+    lifeElement.innerText = 5;
+    const currentScoreElement = document.getElementById('score');
+    currentScoreElement.innerText = 0;
+
+}
+// game over function
+function gameOver() {
+    hideElementById('playground');
+    showElementById('score-screen');
+    const lastScoreElement = document.getElementById('score');
+    const lastScore = lastScoreElement.innerText;
+    const finalScore = document.getElementById('final-score');
+    finalScore.innerText = lastScore;
+}
+
 document.addEventListener('keyup', handleKeyPress)
 
 
